8bcb98e86cf7d29fa166b33e1c96fd584f6380e0
Austin West <korruptedanomaly@gmail.com>
2019-05-13 10:41:22 -0600

    Organized Hierarchy and added SWAPI view controller

---
+++ DeckOfOneCard/View Controllers/SWAPIViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SWAPIViewController.swift
+//  DeckOfOneCard
+//
+//  Created by Austin West on 5/13/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+import UIKit
+
+class SWAPIViewController: UIViewController {
+    
+    let baseURL = "https://swapi.co/api/"
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
